61ad2699a1027c353af17fa9f47e41dabeedad48
Alex Vega <alexsantiagovega99@gmail.com>
2023-04-27 12:35:46 -0300

    gamecard component placed inside the grid

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 
 interface GameProps {
   game: Game;
@@ -13,6 +13,9 @@ const GameCard = ({ game }: GameProps) => {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
+        {game.parent_platforms.map((platform) => (
+          <Text>{platform.platform.name}</Text>
+        ))}
       </CardBody>
     </Card>
   );
