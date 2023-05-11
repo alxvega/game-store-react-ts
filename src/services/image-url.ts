3a04e73ca377d18960689242938e2ee5644efa17
Alex <alexsantiagovega99@gmail.com>
2023-05-11 16:49:52 -0300

    added noimage placeholder

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
-"https://media.rawg.io/media/games/46d/46d98e6910fbc0706e2948a7cc9b10c5.jpg";
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
